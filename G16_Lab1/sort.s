7897dab9df8bff9cead6398181011b0fdfad6d9a
Yuxiang (Mark) Ma <35381478+yma67@users.noreply.github.com>
2019-02-01 17:26:47 -0500

    Update sort.s

--- G16_Lab1/sort.s
+++ G16_Lab1/sort.s
@@ -1,24 +1,24 @@
-			.text
-			.global _start
+		.text
+		.global _start
 _start:
-		LDR	R4, =N
-		MOV R0, #0
+		LDR	R4, =N	// R4: start of program
+		MOV R0, #0	// R0: bool isSorted
 
-WHILE: 	CMP R0, #0
+WHILE: 		CMP R0, #0	// while if not sorted
 		BNE FINISH
-		MOV R0, #1
-		LDR R2, [R4]
-		ADD R3, R4, #4
-		LDR R6, [R3]
+		MOV R0, #1	// set sorted
+		LDR R2, [R4]	// R2: decrement counter
+		ADD R3, R4, #4	// R3: array pointer
+		LDR R6, [R3]	// R6: current element
 
-FOR: 	MOV R5, R6
+FOR: 		MOV R5, R6	// R5: previous element
 		ADD R3, R3, #4
-		SUBS R2, R2, #1
+		SUBS R2, R2, #1 // loop N - 1 times
 		BEQ CNT
 		LDR R6, [R3]
-		CMP R6, R5
-		BGE FOR
-		STR R5, [R3]
+		CMP R6, R5	// if current grater or equal than prev element
+		BGE FOR		// then continue
+		STR R5, [R3]	// else swap in both memory and register and continue
 		STR R6, [R3, #-4]
 		MOV R7, R5
 		MOV R5, R6
@@ -26,8 +26,8 @@ FOR: 	MOV R5, R6
 		MOV R0, #0
 		B FOR
 
-CNT: 	B WHILE
+CNT: 		B WHILE
 
-FINISH: B FINISH
-N: 	.word 5
-NUM: 	.word 5, 4, 3, 2, 1
+FINISH: 	B FINISH
+N: 		.word 5
+NUM: 		.word 5, 4, 3, 2, 1
