30b9d25584d4db6792912fcf8f8d39f62d629fe0
Yuxiang (Mark) Ma <35381478+yma67@users.noreply.github.com>
2019-02-01 16:59:32 -0500

    formatted

--- G16_Lab1/center.s
+++ G16_Lab1/center.s
@@ -1,5 +1,5 @@
-			.text
-			.global _start
+		.text
+		.global _start
 _start:
 		LDR R4, =RESULT
 		LDR R2, [R4, #4]
@@ -8,19 +8,19 @@ _start:
 		ADD R3, R4, #8
 		LDR R5, [R3]
 
-LOOP: 	SUBS R2, R2, #1
+LOOP: 		SUBS R2, R2, #1
 		BEQ NXT
 		ADD R3, R3, #4
 		LDR R1, [R3]
 		ADD R5, R5, R1
 		B LOOP
 
-NXT: 	ASR R5, R5, R8
+NXT: 		ASR R5, R5, R8
 		STR R5, [R4]
 		LDR R2, [R4, #4]
 		ADD R3, R4, #8
 
-MAP: 	LDR R0, [R3]
+MAP: 		LDR R0, [R3]
 		SUBS R0, R0, R5
 		STR R0, [R3], #4
 		SUBS R2, R2, #1
@@ -30,5 +30,5 @@ MAP: 	LDR R0, [R3]
 DONE:	B DONE
 
 RESULT: .word 0
-N: 		.word 8
+N: 	.word 8
 NUMBER: .word 2, 9, 11, 5, 7, 6, 8, 7
