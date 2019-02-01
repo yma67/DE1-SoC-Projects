e46be8bc2058515b16c869713c44a635d46ba30e
Yuxiang (Mark) Ma <35381478+yma67@users.noreply.github.com>
2019-02-01 17:02:44 -0500

    comment

--- G16_Lab1/part1.s
+++ G16_Lab1/part1.s
@@ -1,25 +1,24 @@
-			.text
-			.global _start
+		.text
+		.global _start
 _start:
-			LDR R4, =RESULT
-			LDR R2, [R4, #4] // loop counter 
-			ADD R3, R4, #8 // array pointer
-			LDR R0, [R3] // current max
+		LDR R4, =RESULT
+		LDR R2, [R4, #4] // loop counter 
+		ADD R3, R4, #8   // array pointer
+		LDR R0, [R3]     // current max, as first element in array
 			
-LOOP: 		SUBS R2, R2, #1
-			BEQ DONE
-			ADD R3, R3, #4
-			LDR R1, [R3]
-			CMP R0, R1
-			BGE LOOP
-			MOV R0, R1
-			B LOOP
+LOOP: 		SUBS R2, R2, #1  // counter - 1, loop N - 1 times
+		BEQ DONE	 // end loop
+		ADD R3, R3, #4   // increment array pointer
+		LDR R1, [R3]	 // load current number
+		CMP R0, R1	 // compare element
+		BGE LOOP
+		MOV R0, R1	 // if small, update max
+		B LOOP
 
-DONE: 		STR R0, [R4]
+DONE: 		STR R0, [R4]	 // store output
 
 END: 		B END
 
 RESULT: 	.word 0
-N: 			.word 7
-NUMBERS: 	.word 4, 5, 3, 6
-			.word 1, 8, 2
+N: 		.word 7
+NUMBERS: 	.word 4, 5, 3, 6, 1, 8, 2
