e892a4f02bcdba9f6dbcfe0477c78c8cc783467a
Chris Li <35381733+chrisliii@users.noreply.github.com>
2019-02-20 21:00:41 -0500

    Update maxval.c

--- G16_Lab2_Part2/maxval.c
+++ G16_Lab2_Part2/maxval.c
@@ -1,10 +1,10 @@
 int main(){
-  int a[5] = {1, 20, 3, 4, 5};
-  int max_val;
-  max_val = a[0];
-  for (int i=1; i<5; i++){
-    if (max_val<a[i]){
-      max_val = a[i];
-    }
-  }
+    int a[5] = {1, 20, 3, 4, 5};
+  	int max_val =a[0];
+	  int i;
+  	for (i=1; i<5; i++){
+    	if (max_val<a[i]){
+     	 max_val = a[i];
+    	}
+  	}
 }
