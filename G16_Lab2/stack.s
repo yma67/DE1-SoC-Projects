a315ac60a0c86bde5c646e3290b76bbb9196489a
Yuxiang (Mark) Ma <35381478+yma67@users.noreply.github.com>
2019-02-22 15:40:27 -0500

    Add files via upload

--- G16_Lab2/stack.s
+++ G16_Lab2/stack.s
@@ -2,11 +2,11 @@
 			.global _start
 _start:	
 		LDR R4, =B
-		LDMIA R4!, {R0-R2}
+		LDMIA R4!, {R0-R3}
 		STR R0, [SP, #-4]!
-		PUSH {R1-R2}
-		LDMIA SP!, {R0-R2}
+		STMDB SP!, {R1-R3}
+		LDMIA SP!, {R0-R3}
 
 END: 	B END
 
-B: 		.word 3, 4, 5
+B: 		.word 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
