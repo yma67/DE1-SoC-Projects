cb5004aef0ed30ccff3bb9135c4f7a6957cbc8be
Yuxiang (Mark) Ma <35381478+yma67@users.noreply.github.com>
2019-03-14 14:30:39 -0400

    Add files via upload

---
+++ G16_Lab3/main_intr_timer.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "./drivers/inc/int_setup.h"
+#include "./drivers/inc/ISRs.h"
+#include "./drivers/inc/pushbuttons.h"
+#include "./drivers/inc/HEX_displays.h"
+#include "./drivers/inc/HPS_TIM.h"
+
+int main() {
+	int_setup(1, (int []){199}); 
+    unsigned int count = 0, start = 0;
+
+	HPS_TIM_config_t hps_tim10ms;
+
+	hps_tim10ms.tim = TIM0;
+	hps_tim10ms.timeout = 10000;
+	hps_tim10ms.LD_en = 1;
+	hps_tim10ms.INT_en = 1;
+	hps_tim10ms.enable = 0;
+
+	HPS_TIM_config_ASM(&hps_tim10ms);
+
+	while (1) {
+		if (pushbtn_int_flag == 1) {
+			start = 1;
+			pushbtn_int_flag = 0; 
+		}
+		if (pushbtn_int_flag == 2) {
+			start = 0; 
+			pushbtn_int_flag = 0; 
+		}
+		if (pushbtn_int_flag == 4) {
+			start = 0; 
+			count = 0; 
+			HEX_write_ASM(HEX0 | HEX1 | HEX2 | HEX3 | HEX4 | HEX5, 0);
+			pushbtn_int_flag = 0; 
+		}
+		if (hps_tim0_int_flag) {
+			hps_tim0_int_flag = 0; 
+			if (++count == 366099) count = 0; 
+			HEX_write_ASM(HEX0, count % 10); 
+			HEX_write_ASM(HEX1, (count / 10) % 10); 
+			HEX_write_ASM(HEX2, (count / 100) % 10); 
+			HEX_write_ASM(HEX3, ((count / 100) % 60) / 10);
+			HEX_write_ASM(HEX4, ((count / 100) / 60) % 10);
+			HEX_write_ASM(HEX5, ((count / 100) / 60) / 10 % 10);
+		}
+	}
+    return 0; 
+}
