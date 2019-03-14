6e08cca8e767703c04101a9bafb5646a973deeab
Yuxiang (Mark) Ma <35381478+yma67@users.noreply.github.com>
2019-03-14 11:48:35 -0400

    Add files via upload

--- G16_Lab3/main_polled_timer.c
+++ G16_Lab3/main_polled_timer.c
@@ -14,10 +14,10 @@ int main() {
 	hps_tim10ms.timeout = 10000;
 	hps_tim10ms.LD_en = 1;
 	hps_tim10ms.INT_en = 1;
-	hps_tim10ms.enable = 1;
+	hps_tim10ms.enable = 0;
 
 	hps_tim5ms.tim = TIM1;
-	hps_tim5ms.timeout = 5000;
+	hps_tim5ms.timeout = 3000;
 	hps_tim5ms.LD_en = 1;
 	hps_tim5ms.INT_en = 1;
 	hps_tim5ms.enable = 1;
@@ -28,16 +28,27 @@ int main() {
 	while (1) {
 		if (HPS_TIM_read_INT_ASM(TIM1)) {
 			HPS_TIM_clear_INT_ASM(TIM1);
-			if (PB_edgecap_is_pressed_ASM(0x1)) start = 1; 
-			if (PB_edgecap_is_pressed_ASM(0x2)) start = 0; 
-			if (PB_edgecap_is_pressed_ASM(0x4)) {
+			if (read_PB_edgecap_ASM() & PB0) {
+				PB_clear_edgecap_ASM(PB0); 
+				hps_tim10ms.enable = 1;
+				HPS_TIM_config_ASM(&hps_tim10ms);
+			}
+			if (PB_edgecap_is_pressed_ASM(PB1)) {
+				PB_clear_edgecap_ASM(PB1); 
+				hps_tim10ms.enable = 1;
+				HPS_TIM_config_ASM(&hps_tim10ms);
+			}
+			if (PB_edgecap_is_pressed_ASM(PB2)) {
+				PB_clear_edgecap_ASM(PB2);
+				HPS_TIM_config_ASM(&hps_tim10ms);
+				hps_tim.enable = 0;
 				count = 0; 
 				HEX_clear_ASM(HEX0 | HEX1 | HEX2 | HEX3 | HEX4 | HEX5); 
 			}
 		}
 		if (HPS_TIM_read_INT_ASM(TIM0)) {
 			HPS_TIM_clear_INT_ASM(TIM0); 
-			if (start && ++count == 366099) count = 0; 
+			if (++count == 366099) count = 0; 
 			HEX_write_ASM(HEX0, count % 10); 
 			HEX_write_ASM(HEX1, (count / 10) % 10); 
 			HEX_write_ASM(HEX2, (count / 100) % 10); 
