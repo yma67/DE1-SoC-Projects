7066844fa034a294dac40f46e73c94f57737b2cb
Yuxiang (Mark) Ma <35381478+yma67@users.noreply.github.com>
2020-01-03 01:18:54 -0500

    Update audio.s

--- G16_Lab4/drivers/asm/audio.s
+++ G16_Lab4/drivers/asm/audio.s
@@ -1,27 +1,27 @@
-		.text
-		.equ FIFO_BASE, 0xFF203044
-		.equ LEFT_BASE, 0xFF203048
-		.equ RIGHT_BASE, 0xFF20304C
-		.global write_audio_LR_ASM
+    .text
+    .equ FIFO_BASE, 0xFF203044
+    .equ LEFT_BASE, 0xFF203048
+    .equ RIGHT_BASE, 0xFF20304C
+    .global write_audio_LR_ASM
 
-write_audio_LR_ASM: 
-		
-		PUSH {R1, R2}
-		LDR R1, =FIFO_BASE
-		LDR R2, [R1]
-		TST R2, #0xFF000000
-		BEQ FIN
-		TST R2, #0xFF0000
-		BEQ FIN
-		LDR R1, =LEFT_BASE
-		STR R0, [R1]
-		LDR R1, =RIGHT_BASE
-		STR R0, [R1]
-		MOV R0, #1
-		POP {R1, R2}
-		BX LR
-FIN:	MOV R0, #0
-		POP {R1, R2}
-		BX LR
-
-		.end
+write_audio_LR_ASM: 	
+    PUSH {R1, R2}
+    LDR R1, =FIFO_BASE
+    LDR R2, [R1]
+    TST R2, #0xFF000000
+    BEQ FIN
+    TST R2, #0xFF0000
+    BEQ FIN
+    LDR R1, =LEFT_BASE
+    STR R0, [R1]
+    LDR R1, =RIGHT_BASE
+    STR R0, [R1]
+    MOV R0, #1
+    POP {R1, R2}
+    BX LR
+FIN:
+    MOV R0, #0
+    POP {R1, R2}
+    BX LR
+    
+    .end
