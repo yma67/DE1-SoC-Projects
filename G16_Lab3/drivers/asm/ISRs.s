a92c8b38bb9f347b7a54ca29cace8986523822ce
Yuxiang (Mark) Ma <35381478+yma67@users.noreply.github.com>
2020-01-03 00:57:40 -0500

    Update ISRs.s

--- G16_Lab3/drivers/asm/ISRs.s
+++ G16_Lab3/drivers/asm/ISRs.s
@@ -1,35 +1,35 @@
-	.text
-	
-	.global A9_PRIV_TIM_ISR
-	.global HPS_GPIO1_ISR
-	.global HPS_TIM0_ISR
-	.global HPS_TIM1_ISR
-	.global HPS_TIM2_ISR
-	.global HPS_TIM3_ISR
-	.global FPGA_INTERVAL_TIM_ISR
-	.global FPGA_PB_KEYS_ISR
-	.global FPGA_Audio_ISR
-	.global FPGA_PS2_ISR
-	.global FPGA_JTAG_ISR
-	.global FPGA_IrDA_ISR
-	.global FPGA_JP1_ISR
-	.global FPGA_JP2_ISR
-	.global FPGA_PS2_DUAL_ISR
+    .text
+
+    .global A9_PRIV_TIM_ISR
+    .global HPS_GPIO1_ISR
+    .global HPS_TIM0_ISR
+    .global HPS_TIM1_ISR
+    .global HPS_TIM2_ISR
+    .global HPS_TIM3_ISR
+    .global FPGA_INTERVAL_TIM_ISR
+    .global FPGA_PB_KEYS_ISR
+    .global FPGA_Audio_ISR
+    .global FPGA_PS2_ISR
+    .global FPGA_JTAG_ISR
+    .global FPGA_IrDA_ISR
+    .global FPGA_JP1_ISR
+    .global FPGA_JP2_ISR
+    .global FPGA_PS2_DUAL_ISR
 
     .global hps_tim0_int_flag
     .global pushbtn_int_flag
 
 hps_tim0_int_flag:  
-	.word 0x0
+    .word 0x0
 
 pushbtn_int_flag:   
-	.word 0x0
+    .word 0x0
 
 A9_PRIV_TIM_ISR:
-	BX LR
+    BX LR
 	
 HPS_GPIO1_ISR:
-	BX LR
+    BX LR
 	
 HPS_TIM0_ISR:
     PUSH {LR}
@@ -39,19 +39,19 @@ HPS_TIM0_ISR:
     MOV R1, #1
     STR R1, [R0]
     POP {LR}
-	BX LR
+    BX LR
 	
 HPS_TIM1_ISR:
-	BX LR
+    BX LR
 	
 HPS_TIM2_ISR:
-	BX LR
+    BX LR
 	
 HPS_TIM3_ISR:
-	BX LR
+    BX LR
 	
 FPGA_INTERVAL_TIM_ISR:
-	BX LR
+    BX LR
 	
 FPGA_PB_KEYS_ISR:
     PUSH {R1, LR}
@@ -63,24 +63,24 @@ FPGA_PB_KEYS_ISR:
     BX LR
 	
 FPGA_Audio_ISR:
-	BX LR
+    BX LR
 	
 FPGA_PS2_ISR:
-	BX LR
+    BX LR
 	
 FPGA_JTAG_ISR:
-	BX LR
+    BX LR
 	
 FPGA_IrDA_ISR:
-	BX LR
+    BX LR
 	
 FPGA_JP1_ISR:
-	BX LR
+    BX LR
 	
 FPGA_JP2_ISR:
-	BX LR
+    BX LR
 	
 FPGA_PS2_DUAL_ISR:
-	BX LR
-	
-	.end
+    BX LR
+    
+    .end
